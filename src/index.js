bacbf82b6310519ad13571013749bbcae78098e7
Veronika Pakeleva <veronika.pakeleva@gmail.com>
2022-08-13 02:54:41 +0300

    code vers 2

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 
 module.exports = function towelSort (matrix) {
-  return [];
-
-  let towel = [];
-
-  if (Array.isArray(matrix) && matrix.length){
-
-    for (i=0; i<matrix.length; i++){
-
-      if (i%2 !== 0) {
-        matrix[i] = matrix[i].reverse();
-      }
-
-      let add = matrix[i];
-
-      for (j=0; j<add.length; j++){
-          towel.push(add[j]);
-      }      
-    }
+ 
+  if (matrix && matrix.length == 0) {
+    return [];
+  } else if (matrix && matrix.length == 2) {
+    const arr = matrix[1].reverse();
+    const b = matrix[0].concat(...arr);
+    return b;
+  } else if (matrix && matrix.length == 3) {
+    const arr = matrix[1].reverse();
+    const b = matrix[0].concat(...arr, matrix[2]);
+    return b.flat();
+  } else if (matrix && matrix.length == 4) {
+    const arr = [matrix[0], matrix[1].reverse(), matrix[2], matrix[3].reverse()];
+    arrConcat = Array.prototype.concat(...arr);
+    return arrConcat;
+  } else {
+    return [];
   }
-
-  return towel;
 }
