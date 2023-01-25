249b475abedb0511831277efc2f98cb93f638194
Veronika Pakeleva <94725013+Nika0711@users.noreply.github.com>
2023-01-26 00:01:54 +0300

    feat: add alternative solution

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 
-module.exports = function towelSort (matrix) {
- 
-  if (matrix && matrix.length == 0) {
-    return [];
-  } else if (matrix && matrix.length == 2) {
-    const arr = matrix[1].reverse();
-    const b = matrix[0].concat(...arr);
-    return b;
-  } else if (matrix && matrix.length == 3) {
-    const arr = matrix[1].reverse();
-    const b = matrix[0].concat(...arr, matrix[2]);
-    return b.flat();
-  } else if (matrix && matrix.length == 4) {
-    const arr = [matrix[0], matrix[1].reverse(), matrix[2], matrix[3].reverse()];
-    arrConcat = Array.prototype.concat(...arr);
-    return arrConcat;
-  } else {
-    return [];
-  }
+module.exports = function towelSort(matrix) {
+    if (!matrix) return []
+
+    return matrix.reduce((accumulator, element, index) => {
+        if (index % 2 == 0) accumulator.push(...element)
+        else accumulator.push(...element.reverse())
+        return accumulator
+    }, [])
 }
